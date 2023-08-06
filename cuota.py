ad8c2680ae2a354d210a4ba123f7e77f413fc66f
adanlg <123789087+adanlg@users.noreply.github.com>
2023-08-06 05:12:44 +0200

    feat: add hipoteticas cuotas justas

--- cuota.py
+++ cuota.py
@@ -6,9 +6,9 @@ def verificar_valor_mayor_que_uno(cuota_sum):
     return cuota_sum >= 1
 
 # Ejemplo de uso
-cuota1 = 9.6
-cuota2 = 5.7
-cuota3 = 1.3
+cuota1 = 1.27
+cuota2 = 5.70
+cuota3 = 9.21
 
 odds_sum_resultado = calcular_odds_sum(cuota1, cuota2, cuota3)
 es_mayor_que_uno = verificar_valor_mayor_que_uno(odds_sum_resultado)
@@ -18,3 +18,24 @@ if es_mayor_que_uno:
     print("El valor es mayor o igual que 1. Mal.")
 else:
     print("El valor es menor que 1. Bien.")
+    
+odd1 = 1 / cuota1
+odd2 = 1 / cuota2
+odd3 = 1 / cuota3
+print(odds_sum_resultado)
+print(odd1)
+porcentaje1 = odd1 / odds_sum_resultado
+porcentaje2 = odd2 / odds_sum_resultado
+porcentaje3 = odd3 / odds_sum_resultado
+
+cuotaJusta1 = 1 / porcentaje1
+cuotaJusta2 = 1 / porcentaje2
+cuotaJusta3 = 1 / porcentaje3
+
+comprobacion = porcentaje1 + porcentaje2 + porcentaje3
+
+print(comprobacion)
+print(porcentaje1)
+print(cuotaJusta1)
+print(cuotaJusta2)
+print(cuotaJusta3)
